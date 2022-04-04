900b6f995d51ee433b1103f022a0061d38f0cf6d
makssmeinart <m.meinarts10@gmail.com>
2022-04-04 11:07:26 +0300

    auth reducers thunk refactored

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,9 +1,9 @@
 import {
-  authAPI, FieldsErrorsType,
+  authAPI,
+  FieldsErrorsType,
   LoginParamsType,
   ServerResultCodeResponse,
 } from "../../api/todolists-api";
-import { Dispatch } from "redux";
 import { setAppStatusAC, setIsInitializedAC } from "../../app/app-reducer";
 import {
   handleServerAppError,
@@ -13,9 +13,9 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AxiosError } from "axios";
 
 export const loginTC = createAsyncThunk<
-  { isLoggedIn: boolean },
+  undefined,
   LoginParamsType,
-  { rejectValue: {errors: Array<string>, fieldsError?: FieldsErrorsType}}
+  { rejectValue: { errors: Array<string>; fieldsError?: FieldsErrorsType } }
 >("auth/login", async (data: LoginParamsType, thunkApi) => {
   const { email, password, rememberMe, captcha } = data;
 
@@ -25,10 +25,9 @@ export const loginTC = createAsyncThunk<
     const res = await authAPI.login(email, password, rememberMe, captcha);
     if (res.data.resultCode === ServerResultCodeResponse.success) {
       thunkApi.dispatch(setAppStatusAC("succeeded"));
-      return { isLoggedIn: true };
+      return;
     } else {
       handleServerAppError(res.data, thunkApi.dispatch);
-      debugger;
       return thunkApi.rejectWithValue({
         errors: res.data.messages,
         fieldsError: res.data.fieldsErrors,
@@ -44,41 +43,46 @@ export const loginTC = createAsyncThunk<
   }
 });
 
-export const logoutTC = () => (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC("loading"));
+export const logoutTC = createAsyncThunk(
+  "auth/logout",
+  async (payload, thunkApi) => {
+    thunkApi.dispatch(setAppStatusAC("loading"));
 
-  authAPI
-    .logout()
-    .then((res) => {
+    try {
+      const res = await authAPI.logout();
       if (res.data.resultCode === 0) {
-        dispatch(setAppStatusAC("succeeded"));
-        dispatch(setIsLoggedInAC({ isLoggedIn: false }));
+        thunkApi.dispatch(setAppStatusAC("succeeded"));
+        return;
       } else {
-        handleServerAppError(res.data, dispatch);
+        handleServerAppError(res.data, thunkApi.dispatch);
       }
-    })
-    .catch((err) => {
-      handleServerNetworkError(err, dispatch);
-    });
-};
+    } catch (err: any) {
+      const error: AxiosError = err;
+      handleServerNetworkError(error, thunkApi.dispatch);
+    }
+  }
+);
 
-export const initializeAppTC = () => (dispatch: Dispatch) => {
-  authAPI
-    .me()
-    .then((res) => {
+export const initializeAppTC = createAsyncThunk(
+  "auth/initializedApp",
+  async (payload, thunkApi) => {
+    try {
+      const res = await authAPI.me();
       if (res.data.resultCode === 0) {
-        dispatch(setIsLoggedInAC({ isLoggedIn: true }));
+        return { isLoggedIn: true };
       } else {
-        handleServerAppError(res.data, dispatch);
+        handleServerAppError(res.data, thunkApi.dispatch);
+        return { isLoggedIn: false };
       }
-    })
-    .catch((err) => {
-      handleServerNetworkError(err, dispatch);
-    })
-    .finally(() => {
-      dispatch(setIsInitializedAC(true));
-    });
-};
+    } catch (err: any) {
+      const error: AxiosError = err;
+      handleServerNetworkError(error, thunkApi.dispatch);
+      return { isLoggedIn: false };
+    } finally {
+      thunkApi.dispatch(setIsInitializedAC(true));
+    }
+  }
+);
 
 const slice = createSlice({
   name: "auth",
@@ -94,14 +98,18 @@ const slice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(loginTC.fulfilled, (state, action) => {
+    builder.addCase(loginTC.fulfilled, (state) => {
+      state.isLoggedIn = true;
+    });
+    builder.addCase(logoutTC.fulfilled, (state) => {
+      state.isLoggedIn = false;
+    });
+    builder.addCase(initializeAppTC.fulfilled, (state, action) => {
       state.isLoggedIn = action.payload.isLoggedIn;
     });
   },
 });
 
 // Action Creators
-
-export const setIsLoggedInAC = slice.actions.setIsLoggedInAC;
 
 export const authReducer = slice.reducer;
